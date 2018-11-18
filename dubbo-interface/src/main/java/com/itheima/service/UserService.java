79392609b204e5e92fd191934fb61869174277cb
itcast <bos@itcast.cn>
2018-11-18 16:42:45 +0800

    接口

---
+++ dubbo-interface/src/main/java/com/itheima/service/UserService.java
@@ -0,0 +1,17 @@
+package com.itheima.service;
+
+/***
+ *
+ * @Author:shenkunlin
+ * @Description:itheima
+ * @date: 2018/10/9 11:22
+ *
+ ****/
+public interface UserService {
+
+    /***
+     * 获取用户名
+     * @return
+     */
+    String getUserName();
+}
